1e6ab63f038c7e349cc55ac7705f76163153622d
Chrstphr <somto565@gmail.com>
2023-08-04 20:45:30 +0200

    connected mongoDB

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const helmet = require('helmet');
-const morgan = require('nodemon')
+const morgan = require('morgan')
 
 dotenv.config();
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected To MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+//middleware
+connectToDatabase();
+app.use(helmet());
+app.use(morgan('common'));
+app.use(express.json());
+
 
 app.listen(8800,()=> {
     console.log('Backend is running!!')
