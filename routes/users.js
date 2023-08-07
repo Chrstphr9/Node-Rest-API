f12197b96ead5659f2186e98479b3810e796c24b
Chrstphr <somto565@gmail.com>
2023-08-07 16:42:25 +0200

    map for user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
+const User = require("../models/User")
 const router = require('express').Router();
 
-router.get('/', (req,res)=>{
-    res.send("This is the user route") 
-})
+//Update User
+//Delete User
+//Get A User
+//Follow A User
+//Unfollow A User
+
+
+
+
+
+// router.get('/', (req,res)=>{
+//     res.send("This is the user route") 
+// })
 
 module.exports = router
