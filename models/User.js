530ccc942fe9463872935390bb80066d6a478cd1
Chrstphr <somto565@gmail.com>
2023-08-15 11:42:18 +0200

    able to update user data

--- models/User.js
+++ models/User.js
@@ -1,45 +1,62 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        require: true,
-        min:3,
-        max:20,
-        unique:true
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
     },
-    email:{
-        type: String,
-        require: true,
-        max: 50,
-        unique: true,
+    email: {
+      type: String,
+      require: true,
+      max: 50,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type: String,
-        default:""
+    profilePicture: {
+      type: String,
+      default: "",
     },
-    coverPicture:{
-        type:String,
-        default:""
+    coverPicture: {
+      type: String,
+      default: "",
     },
-    followers:{
-        type:Array,
-        default:[]
+    followers: {
+      type: Array,
+      default: [],
     },
-    following:{
-        type:Array,
-        default:[]
+    following: {
+      type: Array,
+      default: [],
     },
-    isAdmin:{
-        type:Boolean,
-        default: false
-    }
-},
-{timestamps:true}
-)
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,45 @@
-const User = require("../models/User")
-const router = require('express').Router();
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
+router.get('/',(req,res)=> {
+    res.send('hey its user route')
+})
 //Update User
+
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account Has Been Updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+});
+
 //Delete User
+
 //Get A User
+
 //Follow A User
+
 //Unfollow A User
 
-
-
-
-
 // router.get('/', (req,res)=>{
-//     res.send("This is the user route") 
+//     res.send("This is the user route")
 // })
 
-module.exports = router
+module.exports = router;
