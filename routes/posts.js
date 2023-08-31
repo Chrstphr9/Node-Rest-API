98c3f2b7754583bb669e1bddcb6f29ec9952c6e3
somto <somto565@gmail.com>
2023-08-31 13:18:39 +0100

    able to like a post

--- routes/posts.js
+++ routes/posts.js
@@ -47,7 +47,23 @@ router.delete("/:id", async (req, res) => {
     }
 })
 
-//like a post
+//like a post & unlike a post
+
+router.put("/:id/like", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: {likes: req.body.userId} });
+            res.status(200).json("The Post Has Been Liked");
+        } else {
+            await post.updateOne({ $pull: {likes: req.body.userId } });
+            res.statusCode(200).json("The Post Has Been Disliked")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 //get a post
 //get timeline posts
 
